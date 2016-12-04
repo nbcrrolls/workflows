a5e850cec14235fe6319a655f93eadb5fb0bfa43
pieong <pieong@ucsd.edu>
2016-12-04 03:49:36 -0800

    update the workflow, version 1.1

---
+++ Production/AmberGPUMDSimulation/outputs/test2.sh
@@ -0,0 +1,3 @@
+sed -e 's/igValue/241619/g;s/tempValue/310.0/g;s/dtValue/0.002/g;s/nstlimValue/5000000/g;s/ntprValue/5000/g;s/ntwxValue/5000/g;s/gamma_lnValue/5.0/g;' /home/alany999/TestInput1/confDir/md5_switch.conf > /home/alany999/TestInput1/confDir/md5_switch.conf.usr1
+sed -e 's/igValue/171975/g;s/tempValue/310.0/g;s/dtValue/0.002/g;s/nstlimValue/5000000/g;s/ntprValue/5000/g;s/ntwxValue/5000/g;s/gamma_lnValue/5.0/g;' /home/alany999/TestInput1/confDir/md5_switch.conf > /home/alany999/TestInput1/confDir/md5_switch.conf.usr2
+sed -e 's/igValue/740887/g;s/tempValue/310.0/g;s/dtValue/0.002/g;s/nstlimValue/5000000/g;s/ntprValue/5000/g;s/ntwxValue/5000/g;s/gamma_lnValue/5.0/g;' /home/alany999/TestInput1/confDir/md5_switch.conf > /home/alany999/TestInput1/confDir/md5_switch.conf.usr3
